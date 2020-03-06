c89ddaf2f8a59f316a4c9b96f3d042f97f54557e
Vitor Morelli <vitao.miacri@gmail.com>
2020-03-06 16:54:45 -0300

    Adicionado class de Lista ligada

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ import './data-structures/stack';
 import './data-structures/baseConverter';
 
 import './data-structures/queue';
+
 import './data-structures/deque';
 
 import './data-structures/hot-potato';
 
 import './data-structures/palindrome';
+
+import './data-structures/linked-list';
