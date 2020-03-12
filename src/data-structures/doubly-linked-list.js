ccbe445e82e222a40b5c1dc83faf391ce968499c
Vitor Morelli <vitao.miacri@gmail.com>
2020-03-12 15:46:28 -0300

    Adicionado demais métodos lista duplamente ligada

--- src/data-structures/doubly-linked-list.js
+++ src/data-structures/doubly-linked-list.js
@@ -2,10 +2,24 @@ import LinkedList from './linked-list';
 import { defaultEquals } from '../util';
 import { DoublyNode } from './models/linked-list-models';
 
-export default class DoublyLinkeList extends LinkedList {
+export default class DoublyLinkedList extends LinkedList {
   constructor(equalsFn = defaultEquals) {
     super(equalsFn);
     this.tail = undefined;
+  }
+
+  push(element) {
+    const node = new DoublyNode(element);
+    if (this.head == null) {
+      this.head = node;
+      this.tail = node; // NEW
+    } else {
+      // attach to the tail node // NEW
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+    this.count++;
   }
 
   insert(element, index) {
@@ -38,5 +52,83 @@ export default class DoublyLinkeList extends LinkedList {
       return true;
     }
     return false;
+  }
+
+  removeAt(index) {
+    if (index >= 0 && index < this.count) {
+      let current = this.head;
+      if (index === 0) {
+        this.head = current.next;
+        if (this.count === 1) {
+          this.tail = undefined;
+        } else {
+          this.head.prev = undefined;
+        }
+      } else if (index === this.count - 1) {
+        current = this.tail;
+        this.tail = current.prev;
+        this.tail.next = undefined;
+      } else {
+        current = this.getElementAt(index);
+        const previous = current.prev;
+        previous.next = current.next;
+        current.next.prev = previous;
+      }
+      this.count--;
+      return current.element;
+    }
+    return undefined;
+  }
+
+  indexOf(element) {
+    let current = this.head;
+    let index = 0;
+    while (current != null) {
+      if (this.equalsFn(element, current.element)) {
+        return index;
+      }
+      index++;
+      current = current.next;
+    }
+    return -1;
+  }
+
+  getHead() {
+    return this.head;
+  }
+
+  getTail() {
+    return this.tail;
+  }
+
+  clear() {
+    super.clear();
+    this.tail = undefined;
+  }
+
+  toString() {
+    if (this.head == null) {
+      return '';
+    }
+    let objString = `${this.head.element}`;
+    let current = this.head.next;
+    while (current != null) {
+      objString = `${objString},${current.element}`;
+      current = current.next;
+    }
+    return objString;
+  }
+
+  inverseToString() {
+    if (this.tail == null) {
+      return '';
+    }
+    let objString = `${this.tail.element}`;
+    let previous = this.tail.prev;
+    while (previous != null) {
+      objString = `${objString},${previous.element}`;
+      previous = previous.prev;
+    }
+    return objString;
   }
 }
