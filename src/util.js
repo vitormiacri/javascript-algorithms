8dec643d0219f08b6b29d0c82e8c1b985abcf1b9
Vitor Morelli <vitao.miacri@gmail.com>
2020-04-11 16:25:14 -0300

    Classe Utils

--- src/util.js
+++ src/util.js
@@ -1,3 +1,28 @@
+export const Compare = {
+  LESS_THAN: -1,
+  BIGGER_THAN: 1,
+  EQUALS: 0,
+};
+
+export const DOES_NOT_EXIST = -1;
+
+export function lesserEquals(a, b, compareFn) {
+  const comp = compareFn(a, b);
+  return comp === Compare.LESS_THAN || comp === Compare.EQUALS;
+}
+
+export function biggerEquals(a, b, compareFn) {
+  const comp = compareFn(a, b);
+  return comp === Compare.BIGGER_THAN || comp === Compare.EQUALS;
+}
+
+export function defaultCompare(a, b) {
+  if (a === b) {
+    return Compare.EQUALS;
+  }
+  return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
+}
+
 export function defaultEquals(a, b) {
   return a === b;
 }
@@ -13,6 +38,18 @@ export function defaultToString(item) {
     return `${item}`;
   }
   return item.toString();
+}
+
+export function swap(array, a, b) {
+  [array[a], array[b]] = [array[b], array[a]];
+}
+
+export function reverseCompare(compareFn) {
+  return (a, b) => compareFn(b, a);
+}
+
+export function defaultDiff(a, b) {
+  return Number(a) - Number(b);
 }
 
 export function djb2HashCode(key, toStrFn) {
